8e48dc38e0cee03ac17ce704ff02e1ac73d896e5
Amy Scoville <amyscoville@Lins-MacBook-Pro.local>
2017-07-28 19:57:44 -0600

    Added function addItem(event)

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
-function addListItem(){
-    var inputBox = document.getElementById("enter-list-items");
+$(document).ready(function(){
+    $('#input-box').on('keyup', addItem);
+});
+
+
+//handlers
+function addItem(event){
+    if(event.keyCode === 13){
+        var text = $('#input-box').val();
+        var listItems = text.split(',');
+        for (var i = 0; i < listItems.length; i++) {
+            var li = $('<li>' + listItems[i] + '</li>');
+            $('.display-list').append(li);
+            $('#input-box').val('');
+        } 
+    }
 }
+
+
+//utility
 
 
